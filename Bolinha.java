a6f44975cc6091e128cfa5930014aab5d4133cc4
RobertoBrittes <rbrittesgebauer@gmail.com>
2024-10-01 11:52:21 -0300

    criado o arquivo Bolinha

---
+++ Bolinha.java
@@ -0,0 +1,18 @@
+public class Bolinha {
+    public static void main(String[] args) {
+        imprimirBolinha();
+    }
+
+    public static void imprimirBolinha() {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (j <= i) {
+                    System.out.print("X ");
+                } else {
+                    System.out.print("O ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
