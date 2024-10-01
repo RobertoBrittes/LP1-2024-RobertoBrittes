7105feb37dd728f6da993099238d5393bf590f99
RobertoBrittes <rbrittesgebauer@gmail.com>
2024-10-01 11:56:26 -0300

    Soma de dois valores

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Olá mundo");
+        int i = 5;
+        int j = 65;
+
+        int res = j-i;
+        System.out.println(res);
     }
 }
