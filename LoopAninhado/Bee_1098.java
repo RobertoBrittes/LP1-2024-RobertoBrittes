c36e2f051e5b0a2af15f829f0bcc1e581b4f44ae
RobertoBrittes <rbrittesgebauer@gmail.com>
2024-10-03 17:04:41 -0300

    Criado metodo de sequencia

---
+++ LoopAninhado/Bee_1098.java
@@ -0,0 +1,15 @@
+public class Bee_1098 {
+    public static void main(String[] args) {
+        imprimirSequencia();
+    }
+
+    public static void imprimirSequencia() {
+        float i = 0;
+        while (i < 2.2) {
+            for (int k = 0; k < 3; k++) {
+                
+            }
+            i += 0.2;
+        }
+    }
+}
