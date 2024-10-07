dbb01d7d778a07ffd601563fbd46225e726a51b9
RobertoBrittes <rbrittesgebauer@gmail.com>
2024-10-07 08:16:12 -0300

    s

--- LoopAninhado/Bee_1098.java
+++ LoopAninhado/Bee_1098.java
@@ -6,8 +6,22 @@ public class Bee_1098 {
     public static void imprimirSequencia() {
         float i = 0;
         while (i < 2.2) {
-            for (int k = 0; k < 3; k++) {
-                
+            if (i < 1) {
+                for (float k = 1; k <= 3; k++) {
+                    System.out.printf("I=%f.0  J=%f.0\n", i, (k+i));
+                }
+            } else if (i < 2) {
+                for (float k = 1; k <= 3; k++) {
+                    k--;
+                    System.out.printf("I=%f.0  J=%f.0\n", i, (k+i));
+                    k++;
+                }
+            } else {
+                for (float k = 1; k <= 3; k++) {
+                    k-=2;
+                    System.out.printf("I=%f.0  J=%f.0\n", i, (k+i));
+                    k+=2;
+                }
             }
             i += 0.2;
         }
