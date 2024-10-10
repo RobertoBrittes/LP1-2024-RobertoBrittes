a87b25b7e67464f382a8f1315fef7b179a7b2271
RobertoBrittes <rbrittesgebauer@gmail.com>
2024-10-10 16:00:49 -0300

    v 1

---
+++ LoopAninhado/Bee_1214.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Bee_1214 {
+    final static Scanner Sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int qtdTestes = lerNumInt();
+        int mediaTurma = 0;
+        int percentualAcimaMedia = 0;
+        int qtdPessoasTurma = 0;
+        int soma = 0;
+
+        for (int i = 0; i < qtdTestes; i++) {
+            qtdPessoasTurma = lerNumInt();
+            for (int j = 0; j < qtdPessoasTurma; j++) {
+                
+            }
+        }
+    }
+
+    public static int lerNumInt() {
+        return Sc.nextInt();
+    }
+}
---
+++ LoopAninhado/Bee_2311.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Bee_2311 {
+    final static Scanner Sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int instancia   
+    }
+}
