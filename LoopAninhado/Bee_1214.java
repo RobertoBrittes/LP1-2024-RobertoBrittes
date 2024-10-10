fde363fa8046cbd3e357e9aecf54daf8569ff3be
RobertoBrittes <rbrittesgebauer@gmail.com>
2024-10-10 17:07:14 -0300

    v2

--- LoopAninhado/Bee_1214.java
+++ LoopAninhado/Bee_1214.java
@@ -5,17 +5,55 @@ public class Bee_1214 {
 
     public static void main(String[] args) {
         int qtdTestes = lerNumInt();
-        int mediaTurma = 0;
-        int percentualAcimaMedia = 0;
+        float mediaTurma = 0;
+        float percentualAcimaMedia = 0f;
+        int qtdAcimaMedia = 0;
         int qtdPessoasTurma = 0;
         int soma = 0;
+        int[] nota;
 
         for (int i = 0; i < qtdTestes; i++) {
+            soma = 0;
             qtdPessoasTurma = lerNumInt();
-            for (int j = 0; j < qtdPessoasTurma; j++) {
-                
+            nota = lerVetor(qtdPessoasTurma);
+            for (int j = 0; j < nota.length; j++) {
+                soma += nota[j];
+            }
+            mediaTurma = calcularMediaTurma(soma, qtdPessoasTurma);
+            qtdAcimaMedia = verificarAcimaMedia(nota, mediaTurma);
+            percentualAcimaMedia = calcularpercentualAcimaMedia(qtdPessoasTurma, qtdAcimaMedia);
+            System.out.printf("%.3f %s\n", percentualAcimaMedia, "%");
+        }
+    }
+
+    public static int verificarAcimaMedia(int[] nota, float mediaTurma) {
+        int qtdAcimaMedia = 0;
+        for (int i = 0; i < nota.length; i++) {
+            if (nota[i] > mediaTurma) {
+                qtdAcimaMedia++;
             }
         }
+        return qtdAcimaMedia;
+    }
+
+    public static float calcularMediaTurma(int soma, int qtdPessoasTurma) {
+        float mediaTurma = 0f;
+        mediaTurma = (float)soma/qtdPessoasTurma;
+        return mediaTurma;
+    }
+    
+    public static float calcularpercentualAcimaMedia(int qtdPessoasTurma, int qtdAcimaMedia) {
+        float percentualAcimaMedia = 0f;
+        percentualAcimaMedia = (float)(qtdPessoasTurma/qtdAcimaMedia)*100;
+        return percentualAcimaMedia;
+    }
+
+    public static int[] lerVetor(int qtdPessoasTurma) {
+        int[] vetor = new int[qtdPessoasTurma];
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = lerNumInt();
+        }
+        return vetor;
     }
 
     public static int lerNumInt() {
--- LoopAninhado/Bee_2311.java
+++ LoopAninhado/Bee_2311.java
@@ -4,6 +4,41 @@ public class Bee_2311 {
     final static Scanner Sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int instancia   
+        int qtdCompetidores = (int) lerNumFloat();
+        String nome = null;
+        float dificuldadeSalto = 0;
+        final int QTD_SALTOS = 7;
+        float menorNota = 11f;
+        float maiorNota = -1f;
+        float notaSalto = 0f;
+        float soma = 0f;
+
+        for (int i = 0; i < qtdCompetidores; i++) {
+            menorNota = 11;
+            maiorNota = -1;
+            soma = 0;
+            nome = lerString();
+            dificuldadeSalto = lerNumFloat();
+            for (int cont = 0; cont < QTD_SALTOS; cont++) {
+                notaSalto = lerNumFloat();
+                soma += notaSalto;
+                menorNota = Math.min(menorNota, notaSalto);
+                maiorNota = Math.max(maiorNota, notaSalto);
+            }
+            soma -= menorNota;
+            soma -= maiorNota;
+            soma *= dificuldadeSalto;
+            System.out.print(nome);
+            System.out.printf(" %.2f\n", soma);
+        }
+
+    }
+
+    public static String lerString() {
+        return Sc.next();
+    }
+
+    public static float lerNumFloat() {
+        return Sc.nextFloat();
     }
 }
