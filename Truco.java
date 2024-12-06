b7e23e5491312c68064013395fbc5eb8ab98d6d5
RobertoBrittes <rbrittesgebauer@gmail.com>
2024-12-06 16:06:48 -0300

    trucoooo

--- Truco.java
+++ Truco.java
@@ -8,5 +8,22 @@ public class Truco {
     public static void main(String[] args) {
         int pontuacaoJog = 0;
         int pontuacaoComp = 0;
+
+        boolean[][] cartasBaralho = criarMatrizString(10, 4);
+        cartasBaralho = incicializarMatriz(cartasBaralho, true);
+
+    }
+
+    public static boolean[][] incicializarMatriz(boolean[][] matriz, boolean booleano) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                matriz[i][j] = booleano;
+            }
+        }
+        return matriz;
+    }
+
+    public static boolean[][] criarMatrizString(int l, int c) {
+        return new boolean[l][c];
     }
 }
