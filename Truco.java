be565b8a99f8e3ca453ac35920e951a7d2fcc1e2
RobertoBrittes <rbrittesgebauer@gmail.com>
2024-12-06 14:11:58 -0300

    truco

---
+++ Truco.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Truco {
+    final static Scanner Sc = new Scanner(System.in);
+    final static Random rand = new Random();
+
+    public static void main(String[] args) {
+        int pontuacaoJog = 0;
+        int pontuacaoComp = 0;
+    }
+}
