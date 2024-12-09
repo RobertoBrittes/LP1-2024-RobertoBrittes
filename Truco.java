c578999c1155b47688eda0dff258c9bef543e032
RobertoBrittes <rbrittesgebauer@gmail.com>
2024-12-09 14:14:21 -0300

    adicionado metodo para distribuir carta

--- Truco.java
+++ Truco.java
@@ -7,11 +7,40 @@ public class Truco {
 
     public static void main(String[] args) {
         int pontuacaoJog = 0;
-        int pontuacaoComp = 0;
+        int pontuacaoPc = 0;
 
-        boolean[][] cartasBaralho = criarMatrizString(10, 4);
+        int[][] cartasJog = criarMatrizInt(3, 2);
+        int[][] cartasDupla = criarMatrizInt(3, 2);
+
+        int[][] cartasPc1 = criarMatrizInt(3, 2);
+        int[][] cartasPc2 = criarMatrizInt(3, 2);
+
+        boolean[][] cartasBaralho = criarMatrizBool(10, 4);
         cartasBaralho = incicializarMatriz(cartasBaralho, true);
 
+        cartasJog = distribuirCartas(cartasJog);
+
+    }
+
+    public static int[][] distribuirCartas(int[][] cartas) {
+        for (int i = 0; i < cartas[0].length; i++) {
+            for (int j = 0; j < cartas.length; j++) {
+                if (j == 0) {
+                    cartas[i][j] = rand.nextInt(4);
+                } else {
+                    cartas[i][j] = rand.nextInt(10);
+                }
+            }
+        }
+        return cartas;
+    }
+
+    public static int[][] criarMatrizInt(int l, int c) {
+        return new int[l][c];
+    }
+
+    public static boolean[][] criarMatrizBool(int l, int c) {
+        return new boolean[l][c];
     }
 
     public static boolean[][] incicializarMatriz(boolean[][] matriz, boolean booleano) {
@@ -23,7 +52,4 @@ public class Truco {
         return matriz;
     }
 
-    public static boolean[][] criarMatrizString(int l, int c) {
-        return new boolean[l][c];
-    }
 }
